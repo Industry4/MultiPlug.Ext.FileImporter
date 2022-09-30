3ae3e1bae6bfb0222bd2942853e1171d91a3e84f
4IR.UK <David@DELL1>
2022-09-30 06:25:31 +0100

    Add project files.

---
+++ MultiPlug.Ext.FileImporter/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiPlug.Ext.FileImporter
+{
+    public class Class1
+    {
+    }
+}
