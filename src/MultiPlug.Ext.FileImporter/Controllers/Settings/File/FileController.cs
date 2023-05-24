eef440d9a89fd2adff908e92e1bc5d3c7987d521
Joe Banford-Jones <spartan87xx@gmail.com>
2023-05-24 15:04:39 +0100

    #3

--- src/MultiPlug.Ext.FileImporter/Controllers/Settings/File/FileController.cs
+++ src/MultiPlug.Ext.FileImporter/Controllers/Settings/File/FileController.cs
@@ -12,6 +12,7 @@ namespace MultiPlug.Ext.FileImporter.Controllers.Settings.File
         {
             var Search = Core.Instance.Files.FirstOrDefault(File => File.Guid == id);
 
+
             if (Search == null)
             {
                 return new Response
@@ -25,11 +26,42 @@ namespace MultiPlug.Ext.FileImporter.Controllers.Settings.File
                 {
                     Model = new Models.Settings.File.File
                     {
-                        Guid = Search.Guid
+                        Guid = Search.Guid,
+                        Description = Search.Type
                     },
                     Template = Templates.SettingsFile
                 };
             }
         }
+
+        public Response Post(Models.Settings.File.File thePost)
+        {
+            var Search = Core.Instance.Files.FirstOrDefault(File => File.Guid == thePost.Guid);
+
+
+            if (Search == null)
+            {
+                return new Response
+                {
+                    StatusCode = System.Net.HttpStatusCode.Moved,
+                    Location = Context.Referrer
+                };
+            }
+            else
+            {
+                Search.UpdateProperties(new Models.Components.FileImporter.FileImporterProperties { Type = thePost.Description , Guid = thePost.Guid});
+                return new Response
+                {
+                    Model = new Models.Settings.File.File
+                    {
+                        Guid = Search.Guid,
+                        Description = Search.Type
+                    },
+                    Template = Templates.SettingsFile
+                };
+            }
+        }
+
+
     }
 }
--- src/MultiPlug.Ext.FileImporter/Models/Settings/File/File.cs
+++ src/MultiPlug.Ext.FileImporter/Models/Settings/File/File.cs
@@ -4,5 +4,6 @@ namespace MultiPlug.Ext.FileImporter.Models.Settings.File
     public class File
     {
         public string Guid { get; set; }
+        public string Description { get; set; }
     }
 }
